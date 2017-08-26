50ddea9d802ac92c4e04fe49fb71b7cbf8c66905
Hitesh Thawani <hiteshthawani1995@gmail.com>
2017-08-26 17:33:01 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -71,7 +71,7 @@ REPLACE="
 
 # Construct your own list here, it will overwrite the example
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
-REPLACE="/system/etc/media_profiles.xml
+REPLACE="
 "
 
 ##########################################################################################
